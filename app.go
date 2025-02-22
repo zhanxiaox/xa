90c5f195408bd0d8df3fa7505ff22c95f8980b8d
zhan <346084070@qq.com>
2025-02-22 15:12:58 +0800

    0.1.0

--- app.go
+++ app.go
@@ -1,114 +1,99 @@
 package xa
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"slices"
+	"strings"
 )
 
 type App struct {
-	info     AppInfo
-	commands map[string]*command
-	runtime  runtime
+	meta     Meta
+	commands map[string]*Meta
 }
 
-func New(info AppInfo) *App {
+type Meta struct {
+	Name        string
+	Description string
+	Author      string
+	Contact     string
+	Version     string
+	Usage       string
+	Call        func(*App)
+	Params      []Meta
+}
+
+func New(meta Meta) *App {
 	return &App{
-		info:     info,
-		commands: make(map[string]*command),
-		runtime:  runtime{},
+		meta:     meta,
+		commands: make(map[string]*Meta),
 	}
 }
 
-type AppInfo struct {
-	Name              string
-	Description       string
-	Author            string
-	Contact           string
-	Version           string
-	Usage             string
-	EnableDefaultHelp bool
+func (app *App) Run() {
+	if len(os.Args) < 2 {
+		fmt.Println("No command specified")
+		return
+	}
+	if command, ok := app.commands[os.Args[1]]; ok {
+		command.Call(app)
+	} else {
+		fmt.Println("Unknown command:", os.Args[1])
+	}
 }
 
-type runtime struct {
-	path      string
-	command   string
-	arguments []string
+func (app *App) Command(meta Meta) {
+	app.commands[meta.Name] = &meta
 }
 
-func (app *App) GetAppInfo() AppInfo {
-	return app.info
+func getArgs() []string {
+	if len(os.Args) < 2 {
+		return []string{}
+	}
+	return os.Args[2:]
 }
 
-func (app *App) GetRuntime() (string, string, []string) {
-	return app.runtime.path, app.runtime.command, app.runtime.arguments
-}
-
-func (app *App) HasArgument(argument string) bool {
-	return slices.Contains(app.runtime.arguments, argument)
-}
-
-func (app *App) GetArgument(argument string) string {
-	index := slices.Index(app.runtime.arguments, argument)
-	if index >= 0 && len(os.Args) >= index+1 {
-		return app.runtime.arguments[index+1]
+func (app *App) GetArgsByIndex(index int) string {
+	args := getArgs()
+	if len(args) > index {
+		return args[index]
 	}
 	return ""
 }
 
-func (app *App) GetArgmentByIndex(index int) string {
-	return app.runtime.arguments[index]
-}
-
-func (app *App) Run() {
-	app.runtime.path = os.Args[0]
-	if len(os.Args) > 1 {
-		app.runtime.command = os.Args[1]
-	}
-	if len(os.Args) > 2 {
-		app.runtime.arguments = os.Args[2:]
-	}
-	if app.info.EnableDefaultHelp {
-		app.Command("help", defaultHelp).Desc("Print this default help information")
-	}
-	if command, ok := app.commands[app.runtime.command]; ok {
-		command.call(app)
-	} else {
-		if app.info.EnableDefaultHelp {
-			defaultHelp(app)
-		} else {
-			fmt.Println("Unknown command:", app.runtime.command)
+func (app *App) GetArgsByName(name string) string {
+	args := getArgs()
+	if index := slices.Index(args, name); index >= 0 {
+		if len(args)-1 > index+1 {
+			return args[index+1]
 		}
 	}
+	return ""
 }
 
-func (app *App) Command(name string, call func(*App)) (c *command) {
-	c = &command{name: name, call: call}
-	app.commands[name] = c
-	return
-}
-
-func (app *App) GetCommand(name string) (command, error) {
-	if cmd, ok := app.commands[name]; !ok {
-		return command{}, errors.New("command not found")
-	} else {
-		return *cmd, nil
+func (app *App) GetArgsByEqual(name string) string {
+	args := getArgs()
+	for _, arg := range args {
+		_arg := strings.Split(arg, "=")
+		if len(_arg) == 2 && _arg[0] == name {
+			return _arg[1]
+		}
 	}
+	return ""
 }
 
-func defaultHelp(app *App) {
-	fmt.Println(app.info.Name, app.info.Version)
-	fmt.Println(app.info.Description)
+func Help(app *App) {
+	fmt.Println(app.meta.Name, app.meta.Version)
+	fmt.Println(app.meta.Description)
 	fmt.Println()
 	fmt.Println("USAGE:")
-	fmt.Println(app.info.Usage)
+	fmt.Println(app.meta.Usage)
 	fmt.Println()
 	fmt.Println("OPTIONS:")
 	for _, cmd := range app.commands {
-		fmt.Printf("%-17s %v", cmd.name, cmd.description)
-		for _, argument := range cmd.arguments {
-			fmt.Printf("\n  %-15s %v", argument.name, argument.description)
+		fmt.Printf("%-17s %v", cmd.Name, cmd.Description)
+		for _, param := range cmd.Params {
+			fmt.Printf("\n  %-15s %v", param.Name, param.Description)
 		}
 		fmt.Println()
 	}
